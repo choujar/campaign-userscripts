a23809f78892eb9b4df1980411ed1ecd8770c855
Sahil Choujar <sahil.choujar@gmail.com>
2026-03-20 14:10:46 +1030

    Move Greens to opening, just name on sign-off

--- listmanager.user.js
+++ listmanager.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         List Manager Tweaks
 // @namespace    https://github.com/choujar/campaign-userscripts
-// @version      1.49.9
+// @version      1.50.0
 // @description  UX improvements for List Manager and Rocket
 // @author       Sahil Choujar
 // @match        https://listmanager.greens.org.au/*
@@ -4250,7 +4250,7 @@ The election has now been called! We need people to hand out 'How to Vote' cards
 
         // --- Polling Day shift scraping from Rocket contact page ---
         const CONFIRMATION_TEMPLATE_NAME = 'Reminder + Referral';
-        const CONFIRMATION_TEMPLATE_BODY = `Hi [their name], a friendly reminder that you're on a booth tomorrow at [shifts]. A Greens t-shirt (if you requested one), How-to-Vote cards and great vibes will be there for you. Don't forget to vote yourself too!\n\nDouble your impact \u2014 other parties are out in force and we need to make sure voters see the Greens everywhere. If you know someone who could spare even an hour on a booth tomorrow, send me their name and number (with consent) or share mine: 0434 331 085 ([your name], Greens). Thank you!`;
+        const CONFIRMATION_TEMPLATE_BODY = `Hi [their name], a friendly reminder from the Greens that you're on a booth tomorrow at [shifts]. A Greens t-shirt (if you requested one), How-to-Vote cards and great vibes will be there for you. Don't forget to vote yourself too!\n\nDouble your impact \u2014 other parties are out in force and we need to make sure voters see the Greens everywhere. If you know someone who could spare even an hour on a booth tomorrow, send me their name and number (with consent) or share mine: 0434 331 085 ([your name]). Thank you!`;
 
         function getPollingDayShifts() {
             const headers = document.querySelectorAll('h4');
